862d5bcec27b0f66ca8ec4928aa55f6ff43b143d
nicai <tszy111@126.com>
2017-02-09 13:51:27 +0800

    Initial Commit

---
+++ 图片查看器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片查看器
+//
+//  Created by 李自杨 on 17/2/9.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片查看器/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图片查看器
+//
+//  Created by 李自杨 on 17/2/9.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
